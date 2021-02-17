b7dfe2eb4dfd36b45456170e46936dd646816d87
nelson73 <nelson.concha.canto@gmail.com>
2021-02-16 21:01:20 -0600

    add secibd test

--- test.js
+++ test.js
@@ -11,3 +11,14 @@ test('My first test', async t => {
         // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
 });
+
+test('My second test', async t => {
+    await t
+        .typeText('#developer-name', 'John Smith')
+        .click('#submit-button');
+
+    const articleHeader = await Selector('.result-content').find('h1');
+
+    // Obtain the text of the article header
+    let headerText = await articleHeader.innerText;
+});
